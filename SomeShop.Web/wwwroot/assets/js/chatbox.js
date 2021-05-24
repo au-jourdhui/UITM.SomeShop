cd80931f48f1feb79185a313a561441d40d8c81c
Antoniuk Kateryna <petitefille0910@gmail.com>
2021-05-24 21:39:27 +0300

    Fix size of popup chat

--- SomeShop.Web/wwwroot/assets/js/chatbox.js
+++ SomeShop.Web/wwwroot/assets/js/chatbox.js
@@ -2,14 +2,6 @@
     const chatbox = jQuery;
 
     chatbox(() => {
-        const onScroll = () => {
-            const scroll = window.scrollY;
-            const height = 84 - window.scrollY;
-            chatbox(".chatbox-panel").css({bottom: -scroll + "px", top: scroll + "px"});
-            chatbox(".chatbox-popup").css({bottom: height + "px"});
-        }
-        document.addEventListener('scroll', onScroll, true);
-
         chatbox(".chatbox-open").click(async () => {
                 let userInfo;
                 try {
@@ -41,32 +33,27 @@
                     await registerIntoHub(email, name);
                 }
 
-                onScroll()
                 chatbox(".chatbox-popup, .chatbox-close").fadeIn()
             }
         );
 
         chatbox(".chatbox-close").click(() => {
-                onScroll()
                 chatbox(".chatbox-popup, .chatbox-close").fadeOut()
             }
         );
 
         chatbox(".chatbox-maximize").click(() => {
-            onScroll()
             chatbox(".chatbox-popup, .chatbox-open, .chatbox-close").fadeOut();
             chatbox(".chatbox-panel").fadeIn();
             chatbox(".chatbox-panel").css({display: "flex"});
         });
 
         chatbox(".chatbox-minimize").click(() => {
-            onScroll()
             chatbox(".chatbox-panel").fadeOut();
             chatbox(".chatbox-popup, .chatbox-open, .chatbox-close").fadeIn();
         });
 
         chatbox(".chatbox-panel-close").click(() => {
-            onScroll()
             chatbox(".chatbox-panel").fadeOut();
             chatbox(".chatbox-open").fadeIn();
         });
